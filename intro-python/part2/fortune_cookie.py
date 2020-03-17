37abf48398263c21efa3b70061233fc794442ec6
zqli <zqli@cisco.com>
2020-03-17 15:50:14 +0800

    change 2

--- intro-python/part2/fortune_cookie.py
+++ intro-python/part2/fortune_cookie.py
@@ -34,8 +34,19 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
     # TODO: Create a fortune cookie message by calling generate_fortune() and
     # generate_lucky_numbers() and then composing and returning the fortune
     # cookie's message.
+    The_lucky_numbers=generate_lucky_numbers(how_many_lucky_numbers)
+    #print(The_lucky_numbers)
+    The_fortun_message=[]
+    for The_lucky_number in The_lucky_numbers:
 
-    raise NotImplementedError()
+        The_fortun_message.append((str(The_lucky_number)+'.'+generate_fortune()))
+
+        #print('Your fortune number is {}'.format(The_lucky_number),The_fortun_message)
+    return The_fortun_message
+
+
+
+    #raise NotImplementedError()
 
 
 def main():
